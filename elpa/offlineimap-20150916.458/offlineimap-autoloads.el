780b0926a34e67d90aa1d1a1ffb1701a0fa0e997
phil <phil@beadling.co.uk>
2017-11-23 21:33:36 +0000

    Adding offlineimap

---
+++ elpa/offlineimap-20150916.458/offlineimap-autoloads.el
@@ -0,0 +1,22 @@
+;;; offlineimap-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "offlineimap" "offlineimap.el" (23063 14557
+;;;;;;  810786 449000))
+;;; Generated autoloads from offlineimap.el
+
+(autoload 'offlineimap "offlineimap" "\
+Start OfflineIMAP.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; offlineimap-autoloads.el ends here
