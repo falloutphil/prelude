f76fac7ecdefaacc2a6c6ded85e8b31c70876d2c
falloutphil <phil@beadling.co.uk>
2018-01-13 13:47:40 +0000

    Copy field contents of org table

--- personal/lisp-funcs.el
+++ personal/lisp-funcs.el
@@ -10,3 +10,18 @@
   (org-ctrl-c-minus)
   (org-table-toggle-coordinate-overlays)
   )
+
+(defun org-table-mark-field ()
+  "Mark the current table field."
+  (interactive)
+  ;; Do not try to jump to the beginning of field if the point is already there
+  (when (not (looking-back "|\\s-?"))
+    (org-table-beginning-of-field 1))
+  (set-mark-command nil)
+  (org-table-end-of-field 1))
+
+;; Mark shortcut when in a table
+(bind-keys
+ :map org-mode-map
+ :filter (org-at-table-p)
+ ("S-SPC" . org-table-mark-field))
